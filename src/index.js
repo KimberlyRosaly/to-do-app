1ac2e2c3ca3de14cda6a0d011d4ae75e3ae5159b
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-08-11 03:13:11 +0000

    add comments to begin React Router journey

--- src/index.js
+++ src/index.js
@@ -8,8 +8,12 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <Router>
+    {/* - - - - - - - R E A C T   R O U T E R*/}
+    <Router>  {/* AKA 'BROWSERROUTER' | wrapper for entire application */}
       <App />
     </Router>
+    {/* - - - - - - - R E A C T   R O U T E R*/}
+
   </StrictMode>
 );
+// 
