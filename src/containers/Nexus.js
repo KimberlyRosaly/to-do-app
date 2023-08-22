287971afc04ca33d9f8106d74416133f7ae539e2
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-08-22 02:06:31 +0000

    application's real starting point, aka hub/dashboard, has a dummy form

--- src/containers/Nexus.js
+++ src/containers/Nexus.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navigation from '../components/Navigation'
+import ToDoForm from '../components/forms/ToDoForm'
 
 export default function Nexus() {
     return(<>
-    <Navigation />
-    <h1>NEXUS COMPONENT</h1>
+        <Navigation />
+        <h1>NEXUS COMPONENT</h1>
+        <ToDoForm />
     </>)
 };
