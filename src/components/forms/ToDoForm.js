0d688bfcfb80e676713f0a6628194c68ecdfdaa5
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-08-22 02:04:45 +0000

    nested under components and forms directories, a default function form with a text box an submit button, basic description too

---
+++ src/components/forms/ToDoForm.js
@@ -0,0 +1,19 @@
+import React from 'react'    
+
+export default function ToDoForm() {
+    
+    return(<div>
+
+        {/* TEXT : USER INSTRUCTIONS : CONTEXT DESCRIPTION */}
+        <p>INPUT TO DO LIST ITEM</p>
+
+        {/* TO DO LIST FORM */}
+        <form>
+            {/* TEXT INPUT BOX */}
+            <input type="text" />
+            {/* SUBMIT BUTTON */}
+            <button type="SUBMIT">SUBMIT VALUE</button>
+        </form>
+        
+    </div>)
+};
