2e29597b27a1d569670ea10303b11b8ce53245b9
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-08-26 21:04:07 +0000

    Hub has the component rendering for the relaxing artsy part of the app

--- src/containers/Nexus.js
+++ src/containers/Nexus.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navigation from '../components/Navigation'
 import ToDoForm from '../components/forms/ToDoForm'
+import VideoGame from '../components/VideoGame'
 
 export default function Nexus() {
     return(<>
         <Navigation />
         <h1>NEXUS COMPONENT</h1>
         <ToDoForm />
+
+        <VideoGame />
     </>)
 };
