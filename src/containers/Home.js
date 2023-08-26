74b3044907d5d504dd41dfa0b97a826cb389c547
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-08-26 19:26:29 +0000

    testing

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Signup from '../components/Signup'
 import Login from '../components/Login'
+import Navigation from '../components/Navigation'
 
 // H O M E   P A G E   C O N T A I N E R COMPONENT
 export default function Home() {
     return(<>
+    <Navigation />
+
     <h1>HOME CONTAINER COMPONENT</h1>
 
     <div id='portal'>
