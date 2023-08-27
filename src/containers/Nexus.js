704fc583d513dac7b6e9f6cf031a04f0e200e20d
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-08-27 21:01:37 +0000

    Nexus shows off the canvas element rather than the Pixi.js attempt

--- src/containers/Nexus.js
+++ src/containers/Nexus.js
@@ -2,13 +2,15 @@ import React from 'react'
 import Navigation from '../components/Navigation'
 import ToDoForm from '../components/forms/ToDoForm'
 import VideoGame from '../components/VideoGame'
+import CanvasVideoGame from '../components/CanvasVideoGame'
 
 export default function Nexus() {
     return(<>
         <Navigation />
         <h1>NEXUS COMPONENT</h1>
+        {/* <VideoGame /> */}
+        <CanvasVideoGame />
         <ToDoForm />
 
-        <VideoGame />
     </>)
 };
