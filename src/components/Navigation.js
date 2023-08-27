922e5df08d73a492dea3866aefe1b8424a3a59b1
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-08-27 23:30:09 +0000

    create navigation container and list items to be presented in a grid, the nav buttons are now evenly spaced'ish

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,12 +6,16 @@ export default function Navigation() {
     return(
         <div>
             <hr />
-            <p>A  N A V I G A T I O N  BAR </p>
-            <Link to={'/'}><button>URL ROOT</button></Link>
-            <Link to={'/heart'}><button>HEART IS HOME</button></Link>
-            <Link to={'/logout'}><button>USER LOG OUT</button></Link>
+                <p>A  N A V I G A T I O N  BAR </p>
+                <div class="navigation-container">
+                    <ul class="navigation-list">
+                    <li><Link to={'/'}><button>URL ROOT</button></Link></li>
+                    <li><Link to={'/heart'}><button>HEART IS HOME</button></Link></li>
+                    <li><Link to={'/logout'}><button>USER LOG OUT</button></Link></li>
+                    </ul>
+                </div>
             <hr />
         </div>
     )
 
-}
+} 
