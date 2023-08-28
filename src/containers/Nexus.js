9962e4d5f3b1d4588e9fa6e03664eb39c2a31bc4
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-08-28 15:51:04 +0000

    Dashboard / hub now shows off the SVG VIdeoGame component instead of the other image solutions that were being tested

--- src/containers/Nexus.js
+++ src/containers/Nexus.js
@@ -2,14 +2,16 @@ import React from 'react'
 import Navigation from '../components/Navigation'
 import ToDoForm from '../components/forms/ToDoForm'
 // import VideoGame from '../components/VideoGame'
-import CanvasVideoGame from '../components/CanvasVideoGame'
+// import CanvasVideoGame from '../components/CanvasVideoGame'
+import SVGVidgeoGame from '../components/SVGVideoGame'
 
 export default function Nexus() {
     return(<>
         <Navigation />
         <h1>NEXUS COMPONENT</h1>
+        <SVGVidgeoGame />
         {/* <VideoGame /> */}
-        <CanvasVideoGame />
+        {/* <CanvasVideoGame /> */}
         <ToDoForm />
 
     </>)
