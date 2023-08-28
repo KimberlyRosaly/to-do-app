fbdec67d56fa02a310b33f75b9f3a292f6a344be
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-08-28 15:51:53 +0000

    no more buttons! Just fun Links wrapped in list items for user navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,12 +6,12 @@ export default function Navigation() {
     return(
         <div>
             <hr />
-                <p>A  N A V I G A T I O N  BAR </p>
+
                 <div class="navigation-container">
                     <ul class="navigation-list">
-                    <li><Link to={'/'}><button>URL ROOT</button></Link></li>
-                    <li><Link to={'/heart'}><button>HEART IS HOME</button></Link></li>
-                    <li><Link to={'/logout'}><button>USER LOG OUT</button></Link></li>
+                    <li><Link to={'/'}>URL ROOT</Link></li>
+                    <li><Link to={'/heart'}>HEART IS HOME</Link></li>
+                    <li><Link to={'/logout'}>USER LOG OUT</Link></li>
                     </ul>
                 </div>
             <hr />
