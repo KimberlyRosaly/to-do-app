789ad63929b56a70101b914fb0287cd2d5cdc8be
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-08-28 15:50:07 +0000

    try out a neat video game'y component to display some SVG magic

---
+++ src/components/SVGVideoGame.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function SVGVideoGame() {
+    return(<div >
+            <h3>SVG VIDEO GAME LIVES HERE</h3>
+        <svg class="svg-container" width="300" height="400" xmlns="http://www.w3.org/2000/svg">
+<g>
+ 
+ <g stroke-width="2" id="svg_8">
+  <rect stroke-dasharray="5,5" stroke="#000" id="svg_4" height="157" width="13.99999" y="154.55112" x="145.17247" fill="#663400"/>
+  <rect stroke-dasharray="5,5" stroke="#000" id="svg_5" height="46" width="87" y="72.55113" x="110.17247" fill="#236600"/>
+  <rect stroke-dasharray="5,5" stroke="#000" id="svg_2" height="71" width="87" y="125.55113" x="65.17247" fill="#236600"/>
+  <rect stroke-dasharray="5,5" stroke="#000" id="svg_3" height="86.00001" width="130.99999" y="92.55111" x="91.17247" fill="#1a4901"/>
+  <rect transform="rotate(27.2751 178.523 179.416)" stroke-dasharray="5,5" stroke="#000" id="svg_7" height="103.4334" width="10" y="127.69927" x="173.52274" fill="#663400"/>
+  <rect stroke-dasharray="5,5" stroke="#000" id="svg_6" height="46" width="63" y="103.55113" x="182.17247" fill="#236600"/>
+ </g>
+ <rect id="svg_9" height="23.44834" width="22.06902" y="104.13872" x="104.48264" stroke-dasharray="5,5" stroke="#000" fill="#aaff56"/>
+ <rect stroke="#000" id="svg_11" height="12.41382" width="14.48279" y="114.48357" x="192.75873" stroke-dasharray="5,5" fill="#aaff56"/>
+ <rect stroke="#000" id="svg_12" height="18.62074" width="17.93108" y="131.725" x="69.99979" stroke-dasharray="5,5" fill="#aaff56"/>
+ <rect stroke="#000" id="svg_13" height="8.96554" width="13.10348" y="76.55244" x="124.4827" stroke-dasharray="5,5" fill="#aaff56"/>
+</g>
+</svg>
+
+
+
+
+
+        </div>)
+}
