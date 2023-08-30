79755f1175cead1bc03c237dc600fb4e5257c9d6
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-08-30 02:16:32 +0000

    Tree SVG logic was abstracted away and encapsulated within it's own component file

--- src/components/SVGVideoGame.js
+++ src/components/SVGVideoGame.js
@@ -1,50 +1,13 @@
 // ________________________________________IMPORTs
 import React from 'react'
+import { SVGTree } from '../components/stickers/SVGTree' // BRACKETS INDICATE ARROW FUNCTION EXPORT
 import SVGMoon from '../components/stickers/SVGMoon'
 // ________________________________________IMPORTend
 
-// SVG TREE
-const Tree = () => {
-    return (<> 
- <g>
-    <title>GLOWING BACKGROUND LAYER</title>
-    <g id="svg_1" class="tree-svg">
-        <rect fill="#663400" x="89.97972" y="86.99999" width="13.99999" height="157" id="svg_4"  />
-        <rect fill="#236600" x="54.97972" y="5" width="87" height="46" id="svg_5" ray="5,5"/>
-        <rect fill="#236600" x="9.97972" y="58" width="87" height="71" id="svg_2" ray="5,5"/>
-        <rect fill="#1a4901" x="35.97972" y="24.99998" width="130.99999" height="86.00001" id="svg_3"  />
-        <rect transform="rotate(27.2751 123.33 111.865)" fill="#663400" x="118.32999" y="60.14814" width="10" height="103.4334" id="svg_7"  />
-        <rect fill="#236600" x="126.97972" y="36" width="63" height="46" id="svg_6"  />
-        <rect fill="#aaff56"   x="49.28989" y="36.58759" width="22.06902" height="23.44834" id="svg_9"/>
-        <rect fill="#aaff56"  x="137.56598" y="46.93244" width="14.48279" height="12.41382" id="svg_11" />
-        <rect fill="#aaff56"  x="14.80704" y="64.17387" width="17.93108" height="18.62074" id="svg_12" />
-        <rect fill="#aaff56"  x="69.28995" y="9.00131" width="13.10348" height="8.96554" id="svg_13" />
-    </g>
- </g>
-  <g>
-  <title>TREE ART LAYER</title>
-  <g class="tree-svg-art" id="svg_1">
-   <rect fill="#663400" x="89.97972" y="86.99999" width="13.99999" height="157" id="svg_4" stroke="#000" stroke-dasharray="5,5"/>
-   <rect fill="#236600" x="54.97972" y="5" width="87" height="46" id="svg_5" stroke="#000" stroke-dasharray="5,5"/>
-   <rect fill="#236600" x="9.97972" y="58" width="87" height="71" id="svg_2" stroke="#000" stroke-dasharray="5,5"/>
-   <rect fill="#1a4901" x="35.97972" y="24.99998" width="130.99999" height="86.00001" id="svg_3" stroke="#000" stroke-dasharray="5,5"/>
-   <rect transform="rotate(27.2751 123.33 111.865)" fill="#663400" x="118.32999" y="60.14814" width="10" height="103.4334" id="svg_7" stroke="#000" stroke-dasharray="5,5"/>
-   <rect fill="#236600" x="126.97972" y="36" width="63" height="46" id="svg_6" stroke="#000" stroke-dasharray="5,5"/>
-   <rect fill="#aaff56" stroke="#000" stroke-dasharray="5,5" x="49.28989" y="36.58759" width="22.06902" height="23.44834" id="svg_9"/>
-   <rect fill="#aaff56" stroke-dasharray="5,5" x="137.56598" y="46.93244" width="14.48279" height="12.41382" id="svg_11" stroke="#000"/>
-   <rect fill="#aaff56" stroke-dasharray="5,5" x="14.80704" y="64.17387" width="17.93108" height="18.62074" id="svg_12" stroke="#000"/>
-   <rect fill="#aaff56" stroke-dasharray="5,5" x="69.28995" y="9.00131" width="13.10348" height="8.96554" id="svg_13" stroke="#000"/>
-  </g>
- </g>
-    </>);
-};
 
 export default function SVGVideoGame() {
-    return(<div>   
-            <svg className="svg-container" width="200" height="250" xmlns="http://www.w3.org/2000/svg">           
-                <Tree />
-            </svg>
-
+    return(<div className="svg-container"> 
+            <SVGTree />
             <SVGMoon />
         </div>);
 }
