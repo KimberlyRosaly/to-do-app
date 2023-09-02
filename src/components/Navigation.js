c7d34e524184d2fc438a2bbc4cd2461454f551bf
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-09-02 17:26:50 +0000

    clean up excess and be more conventional

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,20 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Navigation() {
-
     return(
-        <div>
-            <hr />
-
-                <div class="navigation-container">
-                    <ul class="navigation-list">
-                    <li><Link to={'/'}>URL ROOT</Link></li>
-                    <li><Link to={'/heart'}>HEART IS HOME</Link></li>
-                    <li><Link to={'/logout'}>USER LOG OUT</Link></li>
+                <nav className="navigation-container">
+                    <ul className="navigation-unordered-list">
+                        <li><Link to={'/'}>URL ROOT</Link></li>
+                        <li><Link to={'/heart'}>HEART ♥ IS HOME</Link></li>
+                        <li><Link to={'/logout'}>USER LOG OUT</Link></li>
                     </ul>
-                </div>
-            <hr />
-        </div>
+                </nav>
     )
-
 } 
