a17784f3bf8ffa87783dc8f05774e97d46f13067
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-09-08 18:33:58 +0000

    Extract the header of the Nexus page into it's own component

--- src/containers/Nexus.js
+++ src/containers/Nexus.js
@@ -3,12 +3,12 @@ import Navigation from '../components/Navigation'
 import ToDoForm from '../components/forms/ToDoForm'
 import { SpaceIsland } from '../components/scenes/SpaceIsland'
 import { Footer } from "../components/Footer";
-
+import Heading from '../components/Heading';
+// -------------[ HOME - HUB - DASHBOARD | FOR SIGNED IN USERS ]------------------
 export default function Nexus() {
     return(<>
         <Navigation />
-        <h1>-productivity hub-</h1>
-        <h2>[TO-DO LIST]</h2>
+        <Heading />
         <div style={{display: "flex", width: "100vw"}}>
             <ToDoForm />
             <SpaceIsland />
