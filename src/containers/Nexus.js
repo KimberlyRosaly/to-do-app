e915984ee1f8e16267af6e3b1164219be619c293
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-09-11 04:00:35 +0000

    import required

--- src/containers/Nexus.js
+++ src/containers/Nexus.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Navigation from '../components/Navigation'
 import { Footer } from "../components/Footer";
 import Heading from '../components/Heading';
+import ProductivityHub from '../containers/ProductivityHub'
+
 // -------------[ HOME - HUB - DASHBOARD | FOR SIGNED IN USERS ]------------------
 export default function Nexus() {
     return(<>
