9adeb308c9b8742439ede41ce771b2e163cc0b45
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-09-14 03:30:25 +0000

    sample data to import around the app

---
+++ src/assets/data.js
@@ -0,0 +1,21 @@
+export const data = [
+        {
+            hello: "okay",
+            testing: "123"
+        },
+        {
+            grave: "robber",
+            lich: "king"
+        }
+]
+
+/*
+    REACH INSIDE :
+        BRACKET NOTATION
+        data[0]['hello']
+        DOT NOTATION
+        data[1].grave
+        DESTRUCTURING OBJECT
+        const {grave: itsValue} = data[1];
+            *DIRECTLY EXTRACT NEEDED VALUE
+*/
