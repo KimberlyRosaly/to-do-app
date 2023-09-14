248cb26ebb93656b80c81c21a08c83085075e949
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-09-14 03:40:26 +0000

    docs(data.js): add comments explaining how to access values in the data array

--- src/containers/ProductivityHub.js
+++ src/containers/ProductivityHub.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import { data } from '../assets/data'
+// import { data } from '../assets/data'
 import ToDoForm from '../components/forms/ToDoForm'
 import { SpaceIsland } from '../components/scenes/SpaceIsland'
 
-export default function ProductivityHub() {
+export default function ProductivityHub({data}) {
     return(
         <section style={{display: "flex", width: "100vw"}}>
             <ToDoForm />
             <SpaceIsland />
+            {console.log(data, "Hello!")}
         </section>
     )
 };
