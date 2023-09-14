35c7ab3fb9cfabd873c31d7c3bd992917090ac94
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-09-14 03:31:39 +0000

    User dashboard now accepts props to receive data, sample data is imported

--- src/containers/Nexus.js
+++ src/containers/Nexus.js
@@ -3,13 +3,19 @@ import Navigation from '../components/Navigation'
 import { Footer } from "../components/Footer";
 import Heading from '../components/Heading';
 import ProductivityHub from '../containers/ProductivityHub'
+import { data } from '../assets/data'
 
 // -------------[ HOME - HUB - DASHBOARD | FOR SIGNED IN USERS ]------------------
 export default function Nexus() {
     return(<>
         <Navigation />
         <Heading />
-        <ProductivityHub />
+        {/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-= */}
+        <ProductivityHub propsData={data} /> {/* AKA TO-DO LIST */}
+        {/* IMPORTED DATA OBJECTS 
+                HANDED TO CHILD COMPONENT
+                        VIA PROPS */}
+        {/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-= */}        
         <Footer />
     </>)
 };
