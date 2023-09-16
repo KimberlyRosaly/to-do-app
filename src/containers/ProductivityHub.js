9b60fdad470e597100cf757a4d610bb6b97e4db2
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-09-16 03:13:55 +0000

    start off some logic for state to live in as a parent container containing children with their own behaviors

--- src/containers/ProductivityHub.js
+++ src/containers/ProductivityHub.js
@@ -2,6 +2,17 @@ import React from 'react'
 // import { data } from '../assets/data'
 import ToDoForm from '../components/forms/ToDoForm'
 import { SpaceIsland } from '../components/scenes/SpaceIsland'
+import { useState } from 'react'
+// -----------------------------------------S T A T E
+const [ sceneItems, setSceneItems ] = useState([]);
+// sceneItems = [1,2,3,4,5] //=> ids of all scene items, in appearance order
+const [ completedItems, setCompletedItems ] = useState([]);
+// completedItems = [1,3,7] //=> ids of items user completed
+// -----------------------------------------S T A T E
+// T= COUNT # OF TICKED ITEMS
+// S= COUNT # OF SCENE ITEMS
+// ITERATE THROUGH SCENE ITEMS
+// DISPLAY:TRUE # SCENE ITEMS
 
 export default function ProductivityHub({propsData}) {
     return(
@@ -24,3 +35,5 @@ export default function ProductivityHub({propsData}) {
     // TREE
     // MOON
     // STAR
+
+    // change name - props usage
