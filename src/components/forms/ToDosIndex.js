2ad8eca2db0ff2785043fdee3d4d66db15e36ec5
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-09-24 23:03:10 +0000

    todos will display via its own component, and can take in props to iterate and display data from

--- src/components/forms/ToDosIndex.js
+++ src/components/forms/ToDosIndex.js
@@ -1,10 +1,24 @@
 import React from 'react';
 export default function ToDosIndex({todosData}){
+
+    
+
     return (
-        <h1>
-            FORM INDEX HERE
-            {console.log("TODOSindex :", todosData)}
-            {todosData.map((t) => {console.log(t.entry)})}
-        </h1>
+        <section>
+            <h4>TO DO LIST</h4>
+            {/* ============================================= L I S T ======== */}       
+                    <div className="to-do-list-container">
+                        {todosData.map((todo, index) => (
+                            <div key={index} >
+                                <input type="checkbox" onChange={() => handleCheckboxChange(index)} />
+                                    <br />
+                                    <hr />
+                                    {todo.entry}
+                                    <hr />
+                            </div>
+                            ))}
+                    </div>               
+                {/* ============================================================== */}
+        </section>
     )
-};
+}; 
