78cc3b7019d6b448853fd96aeb0616cfa932dbd8
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-09-24 23:27:41 +0000

    index of todos now has checkboxes that cross off the entry text via CSS / class name, leveraged via state into a completed item

--- src/components/forms/ToDosIndex.js
+++ src/components/forms/ToDosIndex.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 export default function ToDosIndex({todosData}){
-
-    
-
+const [ completedItems, setCompletedItems ] = useState([]);
+const handleCheckboxChange = (index) => {
+    if (completedItems.includes(index)) {
+        setCompletedItems(completedItems.filter((item) => item !== index));
+    } else {
+        setCompletedItems([...completedItems, index]);
+    }
+};
     return (
         <section>
             <h4>TO DO LIST</h4>
-            {/* ============================================= L I S T ======== */}       
+            {/* ============================================= I N D E X ======== */}       
                     <div className="to-do-list-container">
                         {todosData.map((todo, index) => (
-                            <div key={index} >
+                            <div key={index} className={completedItems.includes(index) ? 'completed' : ''} >
                                 <input type="checkbox" onChange={() => handleCheckboxChange(index)} />
-                                    <br />
-                                    <hr />
-                                    {todo.entry}
-                                    <hr />
+                                <br /><hr />{todo.entry}<hr />
                             </div>
-                            ))}
+                        ))}
                     </div>               
                 {/* ============================================================== */}
         </section>
