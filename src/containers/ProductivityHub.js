21862d4e94ebdb2c92648fb56e9b5e9c78483972
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-09-26 03:11:36 +0000

    feat(ProductivityHub): add completedItems state and updateCompletedItems callback

--- src/containers/ProductivityHub.js
+++ src/containers/ProductivityHub.js
@@ -3,10 +3,14 @@ import ToDosIndex from '../components/forms/ToDosIndex'
 import SpaceIsland from '../components/scenes/SpaceIsland.js'
 
 export default function ProductivityHub({todosData}) {
-
+const [completedItems, setCompletedItems] = useState([]);
+// Callback function > update completed items
+const updateCompletedItems = (newCompletedItems) => {
+    setCompletedItems(newCompletedItems);
+};
     return(
         <section style={{display: "flex", width: "100vw"}}>            
-            <ToDosIndex todosData={todosData} /> 
+            <ToDosIndex todosData={todosData} completedItems={completedItems} updateCompletedItems={updateCompletedItems} /> 
             <SpaceIsland />
         </section>
     )
