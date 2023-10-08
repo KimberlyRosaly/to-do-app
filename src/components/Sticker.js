00e5a5b820fbd6b50a4aafa56a8d6f031b9de950
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-10-08 03:53:11 +0000

    refactor(Sticker): add props for art, background, and display

--- src/components/Sticker.js
+++ src/components/Sticker.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-export const Sticker = () => {
+export const Sticker = ({art, background, display}) => {
     return(<>
     {/* STICKER STUFF WILL RENDER HERE */}
+    <svg display={display} width="110" height="115" transform="translate(0,0)">
+        <g>
+            <title>GLOWING BACKGROUND LAYER</title>
+            <g class="tree-svg" id="svg_21">
+                {background}
+            </g>
+        </g>
+                <g>
+            <title>ART LAYER</title>
+            <g stroke-width="4">
+                {art}
+            </g>
+        </g>
+    </svg>
     </>)
 };
