75db399f6744791a9bbe6423c40442b538f4dc7f
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-10-26 03:00:51 +0000

    logging in functional component to display form with input box and submit button

---
+++ src/containers/LogIn.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function LogIn() {
+    return(
+        <>
+            <h1> LOG IN </h1>
+            <main>
+                <section>
+                    <form>
+                        <label>FIELD
+                            <input type="text" />  
+                        </label>                      
+                        <button>SUBMIT</button>
+                    </form>
+                </section>
+            </main>
+        </>
+    )
+};
