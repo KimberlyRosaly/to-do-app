3e7e3e6d2adcaa8c8044618fb1f7082b7500aebf
kra.dev <40582813+KimberlyRosaly@users.noreply.github.com>
2023-11-04 03:46:06 +0000

    skeleton out what should eventually life on the about page: a section about the application and a section on how to contact the developer

--- src/containers/About.js
+++ src/containers/About.js
@@ -5,7 +5,16 @@ export default function About(){
     return(
         <>
             <Navigation />
-            <h1> ABOUT PAGE LIVES HERE</h1>
+            <h1>            ABOUT PAGE HEADING</h1>
+            <h2>ABOUT SECTION: </h2>
+                <ul>
+                <li>APPLICATION DESCRIPTION</li>
+                <li>SPECIFICATIONS</li>
+                </ul>
+            <h2>    CONTACT SECTION:</h2>
+                <ul>
+                <li>EMAIL ADDRESS</li>
+                </ul>  
             <Footer />
         </>
     )
